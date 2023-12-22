ccb6619ea323e0ae553ba89dc528e94004764cb7
Wok <woctezuma@users.noreply.github.com>
2023-12-22 14:50:22 +0100

    Apply ruff (manually)

--- delet.py
+++ delet.py
@@ -2,174 +2,198 @@ import argparse
 import json
 import logging
 import sys
+from pathlib import Path
 
 import requests
 
-from functions import *
-
-# start the logging module
-logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# load the configuration file
-try:
-    with open("config.json") as cfgFile:
-        cfg = json.load(cfgFile)
-except Exception as e:
-    logger.critical(
-        f"Unable to start script (something with config.json is wrong?). Error:{e}",
-    )
-    sys.exit()
-
-# load in optional arguements to resume, to prevent re-iteration over millions of messages
-arg = argparse.ArgumentParser()
-arg.add_argument("-rf", "--resumefrom")
-arg.add_argument("-rc", "--resumechannel")
-arg.add_argument("-lb", "--lookback")
-arg.add_argument("-d", "--dryrun", action="store_true")
-args = vars(arg.parse_args())
-
-if args["dryrun"]:
-    logger.warning(
-        "DRY: Dry run mode enabled. Messages will not be deleted, but the bot will act like it is.",
-    )
-
-# build the discord headers
-discord_headers = {"Authorization": "Bot " + cfg["token"]}
-
-# get all the server channels
-channels_req = requests.get(
-    "https://discordapp.com/api/guilds/" + cfg["guild"] + "/channels",
-    headers=discord_headers,
+from functions import (
+    TIMEOUT_IN_SECONDS,
+    archive_message_csv,
+    discordapi_check_channel_access,
+    discordapi_delete_message,
+    discordapi_get_messages_batch,
+    message_parser_regex,
+    message_parser_regexuser,
+    message_parser_user,
 )
-if channels_req.status_code != 200:
-    logger.critical("Invalid Discord Guild! (check your token and bot permissions?)")
-    sys.exit()
-else:
-    channels_raw = channels_req.json()
 
-# if the --resumechannel flag is set, lets skip to that channel
-if args["resumechannel"]:
-    i = 0
-    for channel in channels_raw:
-        if channel["id"] != str(args["resumechannel"]):
-            logger.info(
-                f"RESUME: Skipping '{channel['id']}' (#{channel['name']}) on loop '{i}'",
-            )
-        else:
-            logger.info(
-                f"RESUME: Resuming on '{channel['id']}' (#{channel['name']}) on loop '{i}'",
-            )
-            break
-        i += 1
-    channels = channels_raw[i:]
-else:
-    channels = channels_raw
 
-# run through the channels, yes this is nested pretty badly don't @ me
-for channel in channels:
-    if channel["type"] == 0:
-        # if the channel is in the ignore list, skip
-        if channel["id"] in cfg["ignore_channels"]:
-            logger.info(
-                f"SKIPPING CHANNEL FROM CFG: {channel['id']} (#{channel['name']})",
-            )
-            continue
+def main():
+    # start the logging module
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
-        if discordapi_check_channel_access(channel["id"], discord_headers):
-            logger.info(f"ALLOWED CHANNEL READ: {channel['id']} (#{channel['name']})")
+    # load the configuration file
+    try:
+        with Path("config.json").open() as cfg_file:
+            cfg = json.load(cfg_file)
+    except FileNotFoundError as e:
+        message = (
+            f"Unable to start script (something with config.json is wrong?). Error:{e}"
+        )
+        logger.critical(message)
+        sys.exit()
 
-            if args["resumefrom"] and args["resumechannel"] == channel["id"]:
-                BEFORE = args["resumefrom"]
+    # load in optional arguements to resume, to prevent re-iteration over millions of messages
+    arg = argparse.ArgumentParser()
+    arg.add_argument("-rf", "--resumefrom")
+    arg.add_argument("-rc", "--resumechannel")
+    arg.add_argument("-lb", "--lookback")
+    arg.add_argument("-d", "--dryrun", action="store_true")
+    args = vars(arg.parse_args())
+
+    if args["dryrun"]:
+        logger.warning(
+            "DRY: Dry run mode enabled. Messages will not be deleted, but the bot will act like it is.",
+        )
+
+    # build the discord headers
+    discord_headers = {"Authorization": "Bot " + cfg["token"]}
+
+    # get all the server channels
+    channels_req = requests.get(
+        "https://discordapp.com/api/guilds/" + cfg["guild"] + "/channels",
+        headers=discord_headers,
+        timeout=TIMEOUT_IN_SECONDS,
+    )
+    if channels_req.ok:
+        channels_raw = channels_req.json()
+    else:
+        logger.critical(
+            "Invalid Discord Guild! (check your token and bot permissions?)",
+        )
+        sys.exit()
+
+    # if the --resumechannel flag is set, lets skip to that channel
+    if args["resumechannel"]:
+        i = 0
+        for channel in channels_raw:
+            if channel["id"] != str(args["resumechannel"]):
+                message = f"RESUME: Skipping '{channel['id']}' (#{channel['name']}) on loop '{i}'"
+                logger.info(message)
+
             else:
-                BEFORE = channel["last_message_id"]
+                message = f"RESUME: Resuming on '{channel['id']}' (#{channel['name']}) on loop '{i}'"
+                logger.info(message)
+                break
+            i += 1
+        channels = channels_raw[i:]
+    else:
+        channels = channels_raw
 
-            history = 0
+    # run through the channels, yes this is nested pretty badly don't @ me
+    for channel in channels:
+        if channel["type"] == 0:
+            # if the channel is in the ignore list, skip
+            if channel["id"] in cfg["ignore_channels"]:
+                message = (
+                    f"SKIPPING CHANNEL FROM CFG: {channel['id']} (#{channel['name']})"
+                )
+                logger.info(message)
+                continue
 
-            while True:
-                logger.info(
-                    "GETTING BATCH OF 100 MESSAGES: LAST ID {} in {}".format(
-                        BEFORE,
+            if discordapi_check_channel_access(channel["id"], discord_headers):
+                message = f"ALLOWED CHANNEL READ: {channel['id']} (#{channel['name']})"
+                logger.info(message)
+
+                if args["resumefrom"] and args["resumechannel"] == channel["id"]:
+                    value_before = args["resumefrom"]
+                else:
+                    value_before = channel["last_message_id"]
+
+                history = 0
+
+                while True:
+                    message = "GETTING BATCH OF 100 MESSAGES: LAST ID {} in {}".format(
+                        value_before,
                         channel["name"],
-                    ),
-                )
-                msgs = discordapi_get_messages_batch(
-                    channel["id"],
-                    BEFORE,
-                    discord_headers,
-                )
-
-                # reloop if we dont get the right api data
-                if msgs is False:
-                    continue
-
-                if msgs is None:
-                    logger.warning(
-                        "DISCORD API ERROR: Recieved None for messages batch, trying again.",
                     )
-                    continue
+                    logger.info(message)
+                    msgs = discordapi_get_messages_batch(
+                        channel["id"],
+                        value_before,
+                        discord_headers,
+                    )
 
-                history = history + len(msgs)
+                    # reloop if we dont get the right api data
+                    if msgs is False:
+                        continue
 
-                for msg in msgs:
-                    LASTID = msg["id"]
-
-                    if cfg["mode"].lower() == "users":
-                        message_result = message_parser_user(msg, cfg["match_users"])
-                    elif cfg["mode"].lower() == "regex":
-                        message_result = message_parser_regex(msg, cfg["match_regex"])
-                    elif cfg["mode"].lower() == "regexuser":
-                        message_result = message_parser_regexuser(
-                            msg,
-                            cfg["match_users"],
-                            cfg["match_regex"],
+                    if msgs is None:
+                        logger.warning(
+                            "DISCORD API ERROR: Recieved None for messages batch, trying again.",
                         )
-                    else:
-                        logger.critical(
-                            "Unsupported mode! (should be set to either users or regex)",
-                        )
-                        sys.exit()
+                        continue
 
-                    # if this is returned to be true from the parsers, we have to act on the message
-                    if message_result:
-                        logger.info(
-                            "MSG FOUND: Reason: {} Message: {}".format(
-                                message_result,
-                                msg["content"].lower(),
-                            ),
-                        )
+                    history = history + len(msgs)
 
-                        if args["dryrun"]:
-                            logger.debug(
-                                "DRY: Message not being deleted. --dryrun enabled!",
+                    for msg in msgs:
+                        value_lastid = msg["id"]
+
+                        if cfg["mode"].lower() == "users":
+                            message_result = message_parser_user(
+                                msg,
+                                cfg["match_users"],
+                            )
+                        elif cfg["mode"].lower() == "regex":
+                            message_result = message_parser_regex(
+                                msg,
+                                cfg["match_regex"],
+                            )
+                        elif cfg["mode"].lower() == "regexuser":
+                            message_result = message_parser_regexuser(
+                                msg,
+                                cfg["match_users"],
+                                cfg["match_regex"],
                             )
                         else:
-                            delet = discordapi_delete_message(
-                                channel["id"],
-                                msg["id"],
-                                discord_headers,
+                            logger.critical(
+                                "Unsupported mode! (should be set to either users or regex)",
                             )
-                            while delet is not True:
+                            sys.exit()
+
+                        # if this is returned to be true from the parsers, we have to act on the message
+                        if message_result:
+                            message = "MSG FOUND: Reason: {} Message: {}".format(
+                                message_result,
+                                msg["content"].lower(),
+                            )
+                            logger.info(message)
+
+                            if args["dryrun"]:
+                                logger.debug(
+                                    "DRY: Message not being deleted. --dryrun enabled!",
+                                )
+                            else:
                                 delet = discordapi_delete_message(
                                     channel["id"],
                                     msg["id"],
                                     discord_headers,
                                 )
+                                while delet is not True:
+                                    delet = discordapi_delete_message(
+                                        channel["id"],
+                                        msg["id"],
+                                        discord_headers,
+                                    )
 
-                        if cfg["archival_enabled"]:
-                            archive_message_csv(channel, msg, cfg["archival_file"])
+                            if cfg["archival_enabled"]:
+                                archive_message_csv(channel, msg, cfg["archival_file"])
 
-                # if we are in limited lookback mode, hit n and move to next channel
-                if args["lookback"] != 0:
-                    if history >= int(args["lookback"]):
-                        logger.info(
-                            f"LOOKBACK ENABLED, LOOPED THROUGH {history} MESSAGES IN {LASTID}",
-                        )
+                    # if we are in limited lookback mode, hit n and move to next channel
+                    if args["lookback"] != 0 and history >= int(args["lookback"]):
+                        message = f"LOOKBACK ENABLED, LOOPED THROUGH {history} MESSAGES IN {value_lastid}"
+                        logger.info(message)
                         break
 
-                # if the LASTID is not changing, we hit the end of the channel
-                if BEFORE == LASTID:
-                    logger.info(f"END OF CHANNEL: LAST ID {LASTID}")
-                    break
-                else:
-                    BEFORE = LASTID
+                    # if the value_lastid is not changing, we hit the end of the channel
+                    if value_before == value_lastid:
+                        message = f"END OF CHANNEL: LAST ID {value_lastid}"
+                        logger.info(message)
+                        break
+
+                    value_before = value_lastid
+
+
+if __name__ == "__main__":
+    main()
--- functions.py
+++ functions.py
@@ -2,8 +2,12 @@ import csv
 import logging
 import re
 import time
+from http import HTTPStatus
+from pathlib import Path
 
 import requests
+
+TIMEOUT_IN_SECONDS = 5
 
 # start the logging module
 logger = logging.getLogger(__name__)
@@ -13,12 +17,14 @@ def discordapi_check_channel_access(channel, header) -> bool:
     request = requests.get(
         "https://discordapp.com/api/channels/" + channel + "/messages?limit=1",
         headers=header,
+        timeout=TIMEOUT_IN_SECONDS,
     )
-    if request.status_code == 200:
-        return True
-    else:
+    is_a_success = request.ok
+
+    if not is_a_success:
         logger.error(request.status_code)
-        return False
+
+    return is_a_success
 
 
 def discordapi_get_messages_batch(channel, before, header):
@@ -28,43 +34,46 @@ def discordapi_get_messages_batch(channel, before, header):
         + "/messages?limit=100&before="
         + before,
         headers=header,
+        timeout=TIMEOUT_IN_SECONDS,
     )
-    if request.status_code == 200:
-        return request.json()
-    elif request.status_code == 429:
+    if request.ok:
+        response = request.json()
+    elif request.status_code == HTTPStatus.TOO_MANY_REQUESTS:
         json = request.json()
         wait_timer = get_cooldown_future(json)
-        logger.warning(
-            f"DISCORD API RATE LIMIT! [getmsgs] - Backing off and retrying in {wait_timer} seconds",
-        )
+        message = f"DISCORD API RATE LIMIT! [getmsgs] - Backing off and retrying in {wait_timer} seconds"
+        logger.warning(message)
         time.sleep(wait_timer)
-        return None
+        response = None
     else:
-        logger.warning(
-            f"DISCORD API PROBLEM! - Got status code {request.status_code}. Waiting 10 seconds",
-        )
+        message = f"DISCORD API PROBLEM! - Got status code {request.status_code}. Waiting 10 seconds"
+        logger.warning(message)
         time.sleep(10)
-        return False
+        response = False
+
+    return response
 
 
 def discordapi_delete_message(channel, message, header) -> bool:
     request = requests.delete(
         "https://discordapp.com/api/channels/" + channel + "/messages/" + message,
         headers=header,
+        timeout=TIMEOUT_IN_SECONDS,
     )
-    if request.status_code == 204:
-        return True
-    elif request.status_code == 429:
-        json = request.json()
-        wait_timer = get_cooldown_future(json)
-        logger.warning(
-            f"DISCORD API RATE LIMIT! [delete] - Backing off and retrying in {wait_timer} seconds",
-        )
-        time.sleep(wait_timer)
-        return False
-    else:
-        logger.error(f"DISCORD API UNKNOWN ERROR! Status code {request.status_code}")
-        return False
+    is_a_success = bool(request.status_code == HTTPStatus.NO_CONTENT)
+
+    if not is_a_success:
+        if request.status_code == HTTPStatus.TOO_MANY_REQUESTS:
+            json = request.json()
+            wait_timer = get_cooldown_future(json)
+            message = f"DISCORD API RATE LIMIT! [delete] - Backing off and retrying in {wait_timer} seconds"
+            logger.warning(message)
+            time.sleep(wait_timer)
+        else:
+            message = f"DISCORD API UNKNOWN ERROR! Status code {request.status_code}"
+            logger.error(message)
+
+    return is_a_success
 
 
 def get_cooldown_future(rsp):
@@ -75,8 +84,7 @@ def get_cooldown_future(rsp):
 def message_parser_user(message, users):
     if message["author"]["id"] in users:
         return message["author"]["id"]
-    else:
-        return False
+    return False
 
 
 # return True if we should delete the message
@@ -84,8 +92,7 @@ def message_parser_regex(message, regex):
     regex_found = re.findall(regex, message["content"].lower())
     if len(regex_found) == 0:
         return False
-    else:
-        return regex_found
+    return regex_found
 
 
 def message_parser_regexuser(message, users, regex):
@@ -93,14 +100,12 @@ def message_parser_regexuser(message, users, regex):
         regex_found = re.findall(regex, message["content"].lower())
         if len(regex_found) == 0:
             return False
-        else:
-            return regex_found
-    else:
-        return False
+        return regex_found
+    return False
 
 
 def archive_message_csv(channel, message, file) -> bool:
-    with open(file, "a", encoding="utf-8", newline="") as f:
+    with Path(file).open("a", encoding="utf-8", newline="") as f:
         f_writer = csv.writer(f)
         f_writer.writerow(
             [
